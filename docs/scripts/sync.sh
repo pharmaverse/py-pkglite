950b500ffedebf2dda94f79f67c48c5c6d70e9a6
Nan Xiao <me@nanx.me>
2025-01-31 00:42:53 -0500

    Use absolute URL logo in README.md

--- docs/scripts/sync.sh
+++ docs/scripts/sync.sh
@@ -35,7 +35,7 @@ for article in get-started; do
 done
 
 # Sync README.md with modified image path for docs/index.md
-awk '{gsub("docs/assets/logo.png", "assets/logo.png"); print}' README.md >docs/index.md
+awk '{gsub("https://github.com/pharmaverse/py-pkglite/raw/main/docs/assets/logo.png", "assets/logo.png"); print}' README.md >docs/index.md
 
 # Sync CHANGELOG.md with docs/changelog.md
 cp CHANGELOG.md docs/changelog.md
